c34946254e15ef1bce880972bd1efcfc9b0d669d
daxAKAhackerman <samdeg555@gmail.com>
2020-03-10 11:18:07 -0400

    added theme

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,7 +16,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 .modal-backdrop:not(.show):not(.fade) {
