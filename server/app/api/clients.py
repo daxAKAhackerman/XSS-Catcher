3fe2f56a2be06012ef04e5cd63e198b753b95fd4
daxAKAhackerman <samdeg555@gmail.com>
2020-03-20 20:10:47 -0400

    removed debugging stuff

--- server/app/api/clients.py
+++ server/app/api/clients.py
@@ -4,13 +4,9 @@ from app.models import Client
 from app.api import bp
 from flask_login import login_required
 
-from config import Config
-
 @bp.route('/clients', methods=['GET'])
 @login_required
 def get_clients():
-
-    print(Config.SECRET_KEY)
 
     client_list = []
 
