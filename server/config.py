722e89de7c37d6a59229fae1e7adad7aee1d8cd3
daxAKAhackerman <48831884+daxAKAhackerman@users.noreply.github.com>
2021-02-02 20:29:56 -0500

    Added password change script

--- server/config.py
+++ server/config.py
@@ -7,9 +7,18 @@ import random
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+if os.environ.get('POSTGRES_DB') and \
+   os.environ.get('POSTGRES_USER') and \
+   os.environ.get('POSTGRES_PASSWORD'):
+    DATABASE_URL = f'postgres://${os.environ.get("POSTGRES_USER")}:${os.environ.get("POSTGRES_PASSWORD")}@db/${os.environ.get("POSTGRES_DB")}'
+else:
+    DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
 class Config(object):
-    SECRET_KEY = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(32))
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = ''.join(random.choice(
+        string.ascii_letters + string.digits) for i in range(32))
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = 'Lax'
--- server/startup.sh
+++ server/startup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+OLD_PASSWORD=FvzZ0a1mxfWWRp9gAeml
+
+if [ "$POSTGRES_PASSWORD" != "$OLD_PASSWORD" ]; then
+    psql -d "postgresql://$POSTGRES_USER:$OLD_PASSWORD@db/$POSTGRES_DB" -c "\q"
+
+    if [ $? -eq 0 ]; then
+        psql -d "postgresql://$POSTGRES_USER:$OLD_PASSWORD@db/$POSTGRES_DB" -c "ALTER USER $POSTGRES_USER WITH ENCRYPTED PASSWORD '$POSTGRES_PASSWORD';"
+    fi
+
+fi
+
 python3 -m flask db upgrade
 python3 -c "import app; tmpapp = app.create_app(); app.models.init_app(tmpapp)"
 waitress-serve --call 'xss:create_app'
