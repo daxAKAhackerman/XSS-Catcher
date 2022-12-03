a70909cdc7025617d281e80ec5352037bcae34cf
Samuel De Grace <sdegrace@poka.io>
2022-12-02 18:53:29 -0800

    WIP

--- server/db_engine_migrate.sh
+++ server/db_engine_migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pg_password=$(head -n 1 $POSTGRES_PASSWORD_FILE)
-pgloader sqlite:///database-backup.db pgsql://$POSTGRES_USER:$pg_password@$POSTGRES_HOSTNAME/$POSTGRES_DB
+pgloader sqlite://./database-backup.db pgsql://$POSTGRES_USER:$pg_password@$POSTGRES_HOSTNAME/$POSTGRES_DB
