600d7dd3a067ce96ff5c2e88cdbf3250f4188cb8
daxAKAhackerman <samdeg555@gmail.com>
2022-12-26 11:47:34 -0500

    Fix tests

--- server/app/notifications.py
+++ server/app/notifications.py
@@ -6,7 +6,6 @@ from datetime import datetime
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from enum import IntEnum
 from typing import Any, Dict
 
 import requests
