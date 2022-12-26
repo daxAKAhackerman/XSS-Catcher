0076ebaeb2a1ec5cb3471a04ebc87cf404d29e4f
daxAKAhackerman <samdeg555@gmail.com>
2022-12-26 00:19:19 -0500

    WIP

--- server/app/models.py
+++ server/app/models.py
@@ -5,6 +5,7 @@ from typing import Dict, List
 
 from app import db, jwt
 from flask import Flask
+from sqlalchemy.sql import expression
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
@@ -94,8 +95,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(128), unique=True, nullable=False)
     password_hash = db.Column(db.Text, nullable=False)
-    first_login = db.Column(db.Boolean, nullable=False, default=True)
-    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    first_login = db.Column(db.Boolean, nullable=False, server_default=expression.true())
+    is_admin = db.Column(db.Boolean, nullable=False, server_default=expression.false())
     mfa_secret = db.Column(db.String(32))
     client = db.relationship("Client", backref="owner", lazy="dynamic")
 
@@ -118,14 +119,15 @@ class Settings(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     smtp_host = db.Column(db.String(256))
     smtp_port = db.Column(db.Integer)
-    starttls = db.Column(db.Boolean, nullable=False, default=False)
-    ssl_tls = db.Column(db.Boolean, nullable=False, default=False)
+    starttls = db.Column(db.Boolean, nullable=False, server_default=expression.false())
+    ssl_tls = db.Column(db.Boolean, nullable=False, server_default=expression.false())
     mail_from = db.Column(db.Text)
     smtp_user = db.Column(db.String(128))
     smtp_pass = db.Column(db.String(128))
     smtp_status = db.Column(db.Boolean)
     mail_to = db.Column(db.Text)
     webhook_url = db.Column(db.Text)
+    webhook_type = db.Column(db.Integer, nullable=False, server_default="0")
 
     def to_dict(self):
         data = {
--- server/app/notifications.py
+++ server/app/notifications.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from enum import IntEnum
 from typing import Any, Dict
 
 import requests
@@ -106,11 +107,29 @@ class EmailTestNotification(EmailNotification):
 
 
 class WebhookNotification(Notification, metaclass=ABCMeta):
+    settings: Settings
+    webhook_type: int
     webhook_url: str
+
+    def __init__(self) -> None:
+        self.settings: Settings = db.session.query(Settings).one_or_none()
+        self.webhook_type = self.settings.webhook_type
+
+    @property
+    def message(self) -> Dict[str, Any]:
+        if self.webhook_type == 1:
+            return self.discord_message
+        else:
+            return self.slack_message
 
     @property
     @abstractmethod
-    def message(self) -> Dict[str, Any]:  # pragma: no cover
+    def slack_message(self) -> Dict[str, Any]:  # pragma: no cover
+        pass
+
+    @property
+    @abstractmethod
+    def discord_message(self) -> Dict[str, Any]:  # pragma: no cover
         pass
 
     def send(self):
@@ -121,12 +140,12 @@ class WebhookXssNotification(WebhookNotification):
     xss: XSS
 
     def __init__(self, xss: XSS) -> None:
-        settings: Settings = db.session.query(Settings).one_or_none()
+        super().__init__()
         self.xss = xss
-        self.webhook_url = xss.client.webhook_url or settings.webhook_url
+        self.webhook_url = xss.client.webhook_url or self.settings.webhook_url
 
     @property
-    def message(self) -> Dict[str, Any]:
+    def slack_message(self) -> Dict[str, Any]:
         return {
             "text": f"XSS Catcher just caught a new {self.xss.xss_type} XSS for client {self.xss.client.name}",
             "blocks": [
@@ -152,11 +171,34 @@ class WebhookXssNotification(WebhookNotification):
             ],
         }
 
+    @property
+    def discord_message(self) -> Dict[str, Any]:
+        return {
+            "content": f"**XSS Catcher just caught a new XSS**",
+            "embeds": [
+                {
+                    "fields": [
+                        {"inline": True, "name": ":bust_in_silhouette: **Client**", "value": self.xss.client.name},
+                        {"inline": True, "name": ":lock: **XSS type**", "value": self.xss.xss_type},
+                        {"inline": True, "name": ":calendar: **Timestamp**", "value": str(datetime.fromtimestamp(self.xss.timestamp))},
+                        {"inline": True, "name": ":globe_with_meridians: **IP address**", "value": self.xss.ip_addr},
+                        {"inline": True, "name": ":label: **Tags**", "value": ", ".join(json.loads(self.xss.tags))},
+                        {"inline": True, "name": ":floppy_disk: **Data collected**", "value": len(json.loads(self.xss.data))},
+                    ],
+                }
+            ],
+        }
+
 
 class WebhookTestNotification(WebhookNotification):
     def __init__(self, webhook_url: str) -> None:
+        super().__init__()
         self.webhook_url = webhook_url
 
     @property
-    def message(self) -> Dict[str, str]:
+    def slack_message(self) -> Dict[str, str]:
         return {"text": "This is a test webhook from XSS catcher. If you are getting this, it's because your webhook configuration works."}
+
+    @property
+    def discord_message(self) -> Dict[str, str]:
+        return {"content": "This is a test webhook from XSS catcher. If you are getting this, it's because your webhook configuration works."}
