c0a24b1b9d013cd26abaa74727b58745dbcc14ab
daxAKAhackerman <samdeg555@gmail.com>
2022-12-29 09:33:30 -0500

    Adjust frontend

--- client/src/components/index/manageUsers/DeleteUser.vue
+++ client/src/components/index/manageUsers/DeleteUser.vue
@@ -1,48 +1,39 @@
 <template>
-  <b-modal
-    ref="deleteUserModal"
-    id="delete-user-modal"
-    title="Are you sure?"
-    hide-footer
-  >
+  <b-modal ref="deleteUserModal" id="delete-user-modal" title="Are you sure?" @hide="$emit('cleanup')" hide-footer>
     <b-form v-on:submit.prevent>
       <div class="text-right">
-        <b-button @click="deleteUser()" variant="outline-danger"
-          >Yes, delete this user</b-button
-        >
-        <b-button
-          @click="$refs.deleteUserModal.hide()"
-          variant="outline-secondary"
-          >Cancel</b-button
-        >
+        <b-button @click="deleteUser()" variant="outline-danger">Yes, delete this user</b-button>
+        <b-button @click="$refs.deleteUserModal.hide()" variant="outline-secondary">Cancel</b-button>
       </div>
     </b-form>
   </b-modal>
 </template>
 
 <script>
-import axios from "axios";
+import axios from "axios"
 
-const basePath = "/api";
+const basePath = "/api"
 
 export default {
   props: ["to_delete"],
   methods: {
     deleteUser() {
-      const path = `${basePath}/user/${this.to_delete}`;
+      const path = `${basePath}/user/${this.to_delete}`
 
       axios
         .delete(path)
         .then((response) => {
-          this.makeToast(response.data.msg, "success");
-          this.$refs.deleteUserModal.hide();
-          this.$emit("get-users");
+          this.makeToast(response.data.msg, "success")
+          this.$refs.deleteUserModal.hide()
+          this.$emit("get-users")
         })
         .catch((error) => {
-          this.handleError(error);
-        });
+          this.handleError(error)
+        })
     },
   },
-};
+}
 </script>
-<style></style>
+<style>
+
+</style>
---
+++ client/src/components/index/manageUsers/ManageApiKeys.vue
@@ -0,0 +1,43 @@
+<template>
+    <b-modal ref="manageApiKeysModal" id="manage-api-keys-modal" :title="`API keys for user ${user.username}`"
+        @show="listApiKeys()" @hide="$emit('cleanup')" hide-footer>
+        <ApiKeysTable :apiKeys="apiKeys" @list-api-keys="listApiKeys()" />
+    </b-modal>
+</template>
+
+<script>
+import axios from "axios"
+
+import ApiKeysTable from '../security/ApiKeysTable'
+
+const basePath = "/api"
+
+export default {
+    props: ["user"],
+    components: {
+        ApiKeysTable
+    },
+    data() {
+        return {
+            apiKeys: []
+        }
+    },
+    methods: {
+        listApiKeys() {
+            const path = `${basePath}/user/${this.user.id}/apikey`
+
+            axios
+                .get(path)
+                .then((response) => {
+                    this.apiKeys = response.data
+                })
+                .catch((error) => {
+                    this.handleError(error)
+                })
+        },
+    },
+};
+</script>
+<style>
+
+</style>
