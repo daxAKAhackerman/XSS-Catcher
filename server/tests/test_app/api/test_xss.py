384fc73bcd0ec13978e91ac3c7ca8405631b99fa
Sam <samdeg555@gmail.com>
2024-06-23 09:01:39 -0400

    Feat - Single Dockerfile (#250)

--- server/tests/test_app/api/test_xss.py
+++ server/tests/test_app/api/test_xss.py
@@ -1,4 +1,5 @@
 import json
+from typing import cast
 
 from app import db
 from app.api.models import XssGenerateModel
@@ -9,6 +10,7 @@ from app.api.xss import (
 from app.models import XSS
 from flask.testing import FlaskClient
 from freezegun import freeze_time
+from pydantic import AnyHttpUrl
 from tests.helpers import create_client, create_xss, login
 
 
@@ -159,7 +161,7 @@ def test___generate_collector_payload_body__given_body_and_client__then_payload_
     client = create_client("test", uid="abcdef")
     body = XssGenerateModel(
         client_id=1,
-        url="http://127.0.0.1",
+        url=cast(AnyHttpUrl, "http://127.0.0.1"),
         xss_type="r",
         code_type="html",
         to_gather=["local_storage"],
@@ -174,14 +176,18 @@ def test___generate_collector_payload_body__given_body_and_client__then_payload_
 
 
 def test___generate_js_grabber_payload_elements__given_body__when_tags__then_tags_and_payload_returned(client_tester: FlaskClient):
-    body = XssGenerateModel(client_id=1, url="http://127.0.0.1", xss_type="r", code_type="html", to_gather=["local_storage"], tags=["tag1"], custom_js="")
+    body = XssGenerateModel(
+        client_id=1, url=cast(AnyHttpUrl, "http://127.0.0.1"), xss_type="r", code_type="html", to_gather=["local_storage"], tags=["tag1"], custom_js=""
+    )
     tags_query_param, joined_and_trimmed_selected_payloads = _generate_js_grabber_payload_elements(body)
     assert tags_query_param == "tags=tag1"
     assert joined_and_trimmed_selected_payloads == 'local_storage="+encodeURIComponent(JSON.stringify(localStorage))'
 
 
 def test___generate_js_grabber_payload_elements__given_body__when_no_tags__then_tags_and_payload_returned(client_tester: FlaskClient):
-    body = XssGenerateModel(client_id=1, url="http://127.0.0.1", xss_type="r", code_type="html", to_gather=["local_storage"], tags=[], custom_js="")
+    body = XssGenerateModel(
+        client_id=1, url=cast(AnyHttpUrl, "http://127.0.0.1"), xss_type="r", code_type="html", to_gather=["local_storage"], tags=[], custom_js=""
+    )
     tags_query_param, joined_and_trimmed_selected_payloads = _generate_js_grabber_payload_elements(body)
     assert tags_query_param == ""
     assert joined_and_trimmed_selected_payloads == 'local_storage="+encodeURIComponent(JSON.stringify(localStorage))'
@@ -189,6 +195,7 @@ def test___generate_js_grabber_payload_elements__given_body__when_no_tags__then_
 
 @freeze_time("2000-01-01")
 def test__client_xss_get__given_request__then_xss_returned(client_tester: FlaskClient):
+    create_client(name="test")
     xss: XSS = create_xss()
     access_token, refresh_token = login(client_tester, "admin", "xss")
     response = client_tester.get(f"/api/xss/{xss.id}", headers={"Authorization": f"Bearer {access_token}"})
@@ -207,6 +214,7 @@ def test__xss_delete__given_xss_id__then_xss_deleted(client_tester: FlaskClient)
 
 
 def test__xss_loot_get__given_xss_id_and_loot_type__then_loot_returned(client_tester: FlaskClient):
+    create_client(name="test")
     xss: XSS = create_xss(data={"cookies": {"Cookie1": "Value1"}})
     access_token, refresh_token = login(client_tester, "admin", "xss")
     response = client_tester.get(f"/api/xss/{xss.id}/data/cookies", headers={"Authorization": f"Bearer {access_token}"})
@@ -227,6 +235,7 @@ def test__xss_loot_delete__given_xss_id_and_loot_type__then_loot_deleted(client_
 @freeze_time("2000-01-01")
 def test__client_xss_get_all__given_no_filter__then_all_xss_returned(client_tester: FlaskClient):
     create_client("test")
+    create_client("test2")
     create_xss(xss_type="stored")
     create_xss(xss_type="reflected", client_id=2)
     access_token, refresh_token = login(client_tester, "admin", "xss")
@@ -241,6 +250,7 @@ def test__client_xss_get_all__given_no_filter__then_all_xss_returned(client_test
 @freeze_time("2000-01-01")
 def test__client_xss_get_all__given_type_and_client_id_filters__then_only_one_xss_returned(client_tester: FlaskClient):
     create_client("test")
+    create_client("test2")
     create_xss(xss_type="stored", tags=["test"])
     create_xss(xss_type="reflected")
     create_xss(xss_type="reflected", client_id=2)
@@ -253,6 +263,7 @@ def test__client_xss_get_all__given_type_and_client_id_filters__then_only_one_xs
 
 def test__client_loot_get__given_no_filter__then_all_loot_returned(client_tester: FlaskClient):
     create_client("test")
+    create_client("test2")
     create_xss(xss_type="stored", data={"cookies": {"Cookie1": "Value1"}})
     create_xss(xss_type="reflected", client_id=2, data={"local_storage": {"Key1": "Value1"}})
     access_token, refresh_token = login(client_tester, "admin", "xss")
@@ -266,6 +277,7 @@ def test__client_loot_get__given_no_filter__then_all_loot_returned(client_tester
 
 def test__client_loot_get__given_client_id_filter__then_only_one_loot_returned(client_tester: FlaskClient):
     create_client("test")
+    create_client("test2")
     create_xss(xss_type="stored", data={"cookies": {"Cookie1": "Value1"}})
     create_xss(xss_type="reflected", client_id=2, data={"local_storage": {"Key1": "Value1"}})
     access_token, refresh_token = login(client_tester, "admin", "xss")
--- server/tests/test_config.py
+++ server/tests/test_config.py
@@ -5,33 +5,16 @@ from unittest import mock
 from config import Config, get_db_url
 
 
-@mock.patch("config.os.path.exists")
-def test__get_db_url__when_sqlite_db_exists__then_sqlite_used(exists_mocker: mock.MagicMock):
-    db_url = get_db_url()
-    assert db_url.startswith("sqlite:///")
-    assert db_url.endswith("/app.db")
-
-
-def test__get_db_url__when_flask_debug__then_sqlite_used():
-    os.environ["FLASK_DEBUG"] = "1"
-    db_url = get_db_url()
-    assert db_url.startswith("sqlite:///")
-    assert db_url.endswith("/app.db")
-
-
-@mock.patch("builtins.open")
-@mock.patch("config.os.path.exists", return_value=False)
-def test__get_db_url__when_in_prod__then_postgres_used(exists_mocker: mock.MagicMock, open_mocker: mock.MagicMock):
+def test__get_db_url__when_not_in_debug__then_postgres_url_returned():
     os.environ["FLASK_DEBUG"] = ""
-    os.environ["POSTGRES_USER"] = "user"
-    os.environ["POSTGRES_HOSTNAME"] = "127.0.0.1"
-    os.environ["POSTGRES_DB"] = "xss"
-    os.environ["POSTGRES_PASSWORD_FILE"] = "/tmp/file.txt"
+    assert get_db_url() == "postgresql://postgres@/postgres"
+    del os.environ["FLASK_DEBUG"]
 
-    open_mocker.return_value.__enter__.return_value.readline.return_value.rstrip.return_value = "pass"
 
-    db_url = get_db_url()
-    assert db_url == "postgresql://user:pass@127.0.0.1/xss"
+def test__get_db_url__when_in_debug__then_postgres_url_returned():
+    os.environ["FLASK_DEBUG"] = "1"
+    assert get_db_url() == "postgresql://postgres:postgres@localhost:5432/postgres"
+    del os.environ["FLASK_DEBUG"]
 
 
 def test__Config__when_flask_debug__then_dev_secret_key_used():
@@ -39,6 +22,7 @@ def test__Config__when_flask_debug__then_dev_secret_key_used():
     config = Config()
     assert config.SECRET_KEY == "A_KEY_ONLY_USED_FOR_DEV"
     assert config.JWT_ACCESS_TOKEN_EXPIRES == 3600
+    del os.environ["FLASK_DEBUG"]
 
 
 def test__Config__when_not_flask_debug__then_random_secret_key_used():
@@ -46,3 +30,4 @@ def test__Config__when_not_flask_debug__then_random_secret_key_used():
     config = Config()
     assert re.match(r"^[a-zA-Z\d]{32}$", config.SECRET_KEY)
     assert config.JWT_ACCESS_TOKEN_EXPIRES == 300
+    del os.environ["FLASK_DEBUG"]
