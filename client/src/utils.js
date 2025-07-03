8051d6f36ef5af1b639948d71bcfd9ebf3659e6d
Samuel De Grace <samdeg555@gmail.com>
2025-07-03 16:14:46 -0400

    Feat - Frontend tweaks (#314)

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,27 +1,28 @@
-export const makeToast = function(message, variant, title = "Notification") {
-    this.$root.$bvToast.toast(message, {
-        title: title,
-        autoHideDelay: 3000,
-        appendToast: false,
-        variant: variant
-    });
+export const makeToast = function (message, variant, title = "Notification") {
+  this.$root.$bvToast.toast(message, {
+    title: title,
+    autoHideDelay: 3000,
+    appendToast: false,
+    variant: variant,
+    toaster: "b-toaster-bottom-right",
+  });
 };
 
-export const handleError = function(error) {
-    if (error.response.status === 422 || error.response.status === 401) {
-        sessionStorage.removeItem("access_token");
-        sessionStorage.removeItem("refresh_token");
-        this.$router.push({ name: "Login" });
+export const handleError = function (error) {
+  if (error.response.status === 422 || error.response.status === 401) {
+    sessionStorage.removeItem("access_token");
+    sessionStorage.removeItem("refresh_token");
+    this.$router.push({ name: "Login" });
+  } else {
+    let content = "";
+    if (error.response.data.validation_error) {
+      content = `${error.response.data.validation_error.body_params[0].loc[0]}: ${error.response.data.validation_error.body_params[0].msg}`;
     } else {
-        let content = "";
-        if (error.response.data.validation_error) {
-            content = `${error.response.data.validation_error.body_params[0].loc[0]}: ${error.response.data.validation_error.body_params[0].msg}`;
-        } else {
-            content = error.response.data.detail || error.response.data.msg;
-        }
-
-        const title = error.response.data.status || "Notification";
-
-        this.makeToast(content, "danger", title);
+      content = error.response.data.detail || error.response.data.msg;
     }
+
+    const title = error.response.data.status || "Notification";
+
+    this.makeToast(content, "danger", title);
+  }
 };
